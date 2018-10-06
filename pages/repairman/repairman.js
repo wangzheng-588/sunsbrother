aadb8857182d54457ee992b58c607a5546e5b6fa
wz <1136918218@qq.com>
2018-10-06 10:26:08 +0800

    维修师傅提现显示

--- pages/repairman/repairman.js
+++ pages/repairman/repairman.js
@@ -1,66 +1,37 @@
 // pages/repairman/repairman.js
+const app = getApp();
+const api = require('../../api/api.js')
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        serName: '',
+        phone: '',
+        balance: 0,
+        canBePresented: 0
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        let that = this;
+        let params = {
+            userId: app.globalData.userId
+        }
+        api._post("/front/serman/getSerManInfo", app.globalData.accessToken, params)
+            .then(res => {
+                console.log(res)
+                if (res.status === 200) {
+                    that.setData({
+                        serName: res.data.serName,
+                        phone: res.data.serPhone,
+                        balance: res.data.serBalance,
+                        canBePresented: res.data.canBePresented
+                    })
+                }
+            })
+    }
 })
